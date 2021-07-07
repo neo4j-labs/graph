6d51815f28479e63bc5a4be0d8d7a9ba12f75814
Martin Junghanns <martin.junghanns@neo4j.com>
2021-07-08 00:03:05 +0200

    Add global TC

--- src/bin.rs
+++ src/bin.rs
@@ -1,4 +1,7 @@
-use graph::{graph::UndirectedCSRGraph, input::EdgeListInput, read_graph, Graph};
+use std::time::Instant;
+
+use graph::{graph::UndirectedCSRGraph, input::EdgeListInput, read_graph, Graph, UndirectedGraph};
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 fn main() {
     let path = std::env::args()
@@ -12,4 +15,42 @@ fn main() {
 
     println!("node count = {}", graph.node_count());
     println!("edge count = {}", graph.edge_count());
+
+    let start = Instant::now();
+    let global_count = tc(&graph);
+    println!(
+        "global count = {}, took {} seconds",
+        global_count,
+        start.elapsed().as_secs()
+    );
+}
+
+fn tc(graph: &UndirectedCSRGraph) -> usize {
+    (0..graph.node_count())
+        .into_par_iter()
+        .map(|u| {
+            let mut triangles = 0;
+
+            for &v in graph.neighbors(u) {
+                if v > u {
+                    break;
+                }
+                let mut it = graph.neighbors(u).iter();
+
+                for &w in graph.neighbors(v) {
+                    if w > v {
+                        break;
+                    }
+
+                    if let Some(x) = it.by_ref().skip_while(|&tmp| *tmp < w).next() {
+                        if *x == w {
+                            triangles += 1;
+                        }
+                    }
+                }
+            }
+
+            triangles
+        })
+        .sum()
 }
