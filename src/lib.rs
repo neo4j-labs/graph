ded4e3f2fd0fb3a5b59019939f3a3cb42098cd7c
Martin Junghanns <martin.junghanns@neo4j.com>
2021-07-21 21:35:32 +0200

    Improve lib tests
    Co-Authored-By: Paul Horn <developer@knutwalker.de>

--- src/lib.rs
+++ src/lib.rs
@@ -89,7 +89,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn read_graph_test() {
+    fn should_compile_test() {
         fn inner_test() -> Result<(), Error> {
             let _g0: DirectedCSRGraph<usize> = read_graph("graph", EdgeListInput::default())?;
             let _g0: DirectedCSRGraph<_> = read_graph("graph", EdgeListInput::<usize>::default())?;
@@ -107,94 +107,96 @@ mod tests {
     }
 
     #[test]
-    fn directed_graph_from_edge_list() {
-        let edge_list = EdgeList::new(vec![(0, 1), (0, 2)]);
-
-        let g: DirectedCSRGraph<usize> = create_graph(edge_list);
-
-        assert_eq!(g.node_count(), 3);
-        assert_eq!(g.edge_count(), 2);
-
-        assert_eq!(g.out_degree(0), 2);
-        assert_eq!(g.out_degree(1), 0);
-        assert_eq!(g.out_degree(2), 0);
-
-        assert_eq!(g.in_degree(0), 0);
-        assert_eq!(g.in_degree(1), 1);
-        assert_eq!(g.in_degree(2), 1);
-
-        assert_eq!(g.out_neighbors(0), &[1, 2]);
-        assert_eq!(g.out_neighbors(1), &[]);
-        assert_eq!(g.out_neighbors(2), &[]);
-
-        assert_eq!(g.in_neighbors(0), &[]);
-        assert_eq!(g.in_neighbors(1), &[0]);
-        assert_eq!(g.in_neighbors(2), &[0]);
+    fn directed_usize_graph_from_edge_list() {
+        let edges = vec![(0, 1), (0, 2)];
+        assert_directed_graph::<usize>(create_graph(EdgeList::new(edges)));
     }
 
     #[test]
-    fn undirected_graph_from_edge_list() {
-        let edge_list = EdgeList::new(vec![(0, 1), (0, 2)]);
-
-        let g: UndirectedCSRGraph<usize> = create_graph(edge_list);
-
-        assert_eq!(g.node_count(), 3);
-        assert_eq!(g.edge_count(), 2);
-
-        assert_eq!(g.degree(0), 2);
-        assert_eq!(g.degree(1), 1);
-        assert_eq!(g.degree(2), 1);
-
-        assert_eq!(g.neighbors(0), &[1, 2]);
-        assert_eq!(g.neighbors(1), &[0]);
-        assert_eq!(g.neighbors(2), &[0]);
+    fn directed_u32_graph_from_edge_list() {
+        let edges = vec![(0, 1), (0, 2)];
+        assert_directed_graph::<u32>(create_graph(EdgeList::new(edges)));
     }
 
     #[test]
-    fn directed_graph_from_edge_list_file() {
+    fn undirected_usize_graph_from_edge_list() {
+        let edge_list = EdgeList::new(vec![(0, 1), (0, 2)]);
+        assert_undirected_graph::<usize>(create_graph(edge_list));
+    }
+
+    #[test]
+    fn undirected_u32_graph_from_edge_list() {
+        let edge_list = EdgeList::new(vec![(0, 1), (0, 2)]);
+        assert_undirected_graph::<u32>(create_graph(edge_list));
+    }
+
+    #[test]
+    fn directed_usize_graph_from_edge_list_file() {
         let path = [env!("CARGO_MANIFEST_DIR"), "resources", "test.el"]
             .iter()
             .collect::<PathBuf>();
 
-        let g: DirectedCSRGraph<usize> = read_graph(path, EdgeListInput::default()).unwrap();
-
-        assert_eq!(g.node_count(), 3);
-        assert_eq!(g.edge_count(), 2);
-
-        assert_eq!(g.out_degree(0), 2);
-        assert_eq!(g.out_degree(1), 0);
-        assert_eq!(g.out_degree(2), 0);
-
-        assert_eq!(g.in_degree(0), 0);
-        assert_eq!(g.in_degree(1), 1);
-        assert_eq!(g.in_degree(2), 1);
-
-        assert_eq!(g.out_neighbors(0), &[1, 2]);
-        assert_eq!(g.out_neighbors(1), &[]);
-        assert_eq!(g.out_neighbors(2), &[]);
-
-        assert_eq!(g.in_neighbors(0), &[]);
-        assert_eq!(g.in_neighbors(1), &[0]);
-        assert_eq!(g.in_neighbors(2), &[0]);
+        assert_directed_graph::<usize>(read_graph(path, EdgeListInput::default()).unwrap());
     }
 
     #[test]
-    fn undirected_graph_from_edge_list_file() {
+    fn directed_u32_graph_from_edge_list_file() {
         let path = [env!("CARGO_MANIFEST_DIR"), "resources", "test.el"]
             .iter()
             .collect::<PathBuf>();
 
-        let g: UndirectedCSRGraph<usize> = read_graph(path, EdgeListInput::default()).unwrap();
+        assert_directed_graph::<u32>(read_graph(path, EdgeListInput::default()).unwrap());
+    }
 
-        assert_eq!(g.node_count(), 3);
-        assert_eq!(g.edge_count(), 2);
+    #[test]
+    fn undirected_usize_graph_from_edge_list_file() {
+        let path = [env!("CARGO_MANIFEST_DIR"), "resources", "test.el"]
+            .iter()
+            .collect::<PathBuf>();
 
-        assert_eq!(g.degree(0), 2);
-        assert_eq!(g.degree(1), 1);
-        assert_eq!(g.degree(2), 1);
+        assert_undirected_graph::<usize>(read_graph(path, EdgeListInput::default()).unwrap());
+    }
 
-        assert_eq!(g.neighbors(0), &[1, 2]);
-        assert_eq!(g.neighbors(1), &[0]);
-        assert_eq!(g.neighbors(2), &[0]);
+    #[test]
+    fn undirected_u32_graph_from_edge_list_file() {
+        let path = [env!("CARGO_MANIFEST_DIR"), "resources", "test.el"]
+            .iter()
+            .collect::<PathBuf>();
+
+        assert_undirected_graph::<u32>(read_graph(path, EdgeListInput::default()).unwrap());
+    }
+
+    fn assert_directed_graph<Node: Idx>(g: DirectedCSRGraph<Node>) {
+        assert_eq!(g.node_count(), Node::new(3));
+        assert_eq!(g.edge_count(), Node::new(2));
+
+        assert_eq!(g.out_degree(Node::new(0)), Node::new(2));
+        assert_eq!(g.out_degree(Node::new(1)), Node::new(0));
+        assert_eq!(g.out_degree(Node::new(2)), Node::new(0));
+
+        assert_eq!(g.in_degree(Node::new(0)), Node::new(0));
+        assert_eq!(g.in_degree(Node::new(1)), Node::new(1));
+        assert_eq!(g.in_degree(Node::new(2)), Node::new(1));
+
+        assert_eq!(g.out_neighbors(Node::new(0)), &[Node::new(1), Node::new(2)]);
+        assert_eq!(g.out_neighbors(Node::new(1)), &[]);
+        assert_eq!(g.out_neighbors(Node::new(2)), &[]);
+
+        assert_eq!(g.in_neighbors(Node::new(0)), &[]);
+        assert_eq!(g.in_neighbors(Node::new(1)), &[Node::new(0)]);
+        assert_eq!(g.in_neighbors(Node::new(2)), &[Node::new(0)]);
+    }
+
+    fn assert_undirected_graph<Node: Idx>(g: UndirectedCSRGraph<Node>) {
+        assert_eq!(g.node_count(), Node::new(3));
+        assert_eq!(g.edge_count(), Node::new(2));
+
+        assert_eq!(g.degree(Node::new(0)), Node::new(2));
+        assert_eq!(g.degree(Node::new(1)), Node::new(1));
+        assert_eq!(g.degree(Node::new(2)), Node::new(1));
+
+        assert_eq!(g.neighbors(Node::new(0)), &[Node::new(1), Node::new(2)]);
+        assert_eq!(g.neighbors(Node::new(1)), &[Node::new(0)]);
+        assert_eq!(g.neighbors(Node::new(2)), &[Node::new(0)]);
     }
 }
