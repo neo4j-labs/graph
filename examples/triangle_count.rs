2085bba860b933d0f8918eab46cb0274d0c157ff
Paul Horn <developer@knutwalker.de>
2021-07-30 11:48:32 +0200

    Have an alternative implementation of tc parallelism

--- examples/triangle_count.rs
+++ examples/triangle_count.rs
@@ -59,36 +59,89 @@ fn global_triangle_count<Node: Idx>(graph: UndirectedCSRGraph<Node>) {
     );
 
     let start = Instant::now();
-    let tc: usize = (0..graph.node_count().index())
-        .into_par_iter()
-        .map(Node::new)
-        .map(|u| {
-            let mut triangles = 0_usize;
-            for &v in graph.neighbors(u) {
-                if v > u {
-                    break;
-                }
-                let mut it = graph.neighbors(u).iter();
 
-                for &w in graph.neighbors(v) {
-                    if w > v {
-                        break;
+    let tc = if std::env::var_os("TC_SCOPED").is_some() {
+        use graph::index::AtomicIdx;
+        use std::sync::atomic::AtomicU64;
+
+        let next_chunk = Node::zero().atomic();
+        let total_triangles = AtomicU64::new(0);
+
+        rayon::scope(|s| {
+            for _ in 0..rayon::current_num_threads() {
+                s.spawn(|_| {
+                    let mut triangles = 0;
+
+                    loop {
+                        let start = next_chunk.fetch_add(64, std::sync::atomic::Ordering::AcqRel);
+                        if start >= graph.node_count() {
+                            break;
+                        }
+
+                        let end = (start + Node::new(64)).min(graph.node_count());
+
+                        for n in start.index()..end.index() {
+                            let u = Node::new(n);
+
+                            for &v in graph.neighbors(u) {
+                                if v > u {
+                                    break;
+                                }
+                                let mut it = graph.neighbors(u).iter();
+
+                                for &w in graph.neighbors(v) {
+                                    if w > v {
+                                        break;
+                                    }
+
+                                    if let Some(x) = it.by_ref().find(|&tmp| *tmp >= w) {
+                                        if *x == w {
+                                            triangles += 1;
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
 
-                    if let Some(x) = it.by_ref().find(|&tmp| *tmp >= w) {
-                        if *x == w {
-                            triangles += 1;
+                    total_triangles.fetch_add(triangles, std::sync::atomic::Ordering::AcqRel);
+                });
+            }
+        });
+
+        total_triangles.load(std::sync::atomic::Ordering::SeqCst)
+    } else {
+        (0..graph.node_count().index())
+            .into_par_iter()
+            .map(Node::new)
+            .map(|u| {
+                let mut triangles = 0;
+                for &v in graph.neighbors(u) {
+                    if v > u {
+                        break;
+                    }
+                    let mut it = graph.neighbors(u).iter();
+
+                    for &w in graph.neighbors(v) {
+                        if w > v {
+                            break;
+                        }
+
+                        if let Some(x) = it.by_ref().find(|&tmp| *tmp >= w) {
+                            if *x == w {
+                                triangles += 1;
+                            }
                         }
                     }
                 }
-            }
-            triangles
-        })
-        .sum();
+                triangles
+            })
+            .sum()
+    };
 
     info!(
-        "Triangle counting finished in {} seconds .. global triangle count = {}",
-        start.elapsed().as_secs(),
+        "Triangle counting finished in {:?} seconds .. global triangle count = {}",
+        start.elapsed(),
         tc
     );
 }
