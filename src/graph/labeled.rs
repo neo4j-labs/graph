74d6b7c9fd396031b37f1871caa88b8435dc086c
Martin Junghanns <martin.junghanns@neo4j.com>
2021-08-19 16:55:33 +0200

    Impl NodeLabeldGraph for NodeLabeledCsrGraph

--- src/graph/labeled.rs
+++ src/graph/labeled.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 
 use crate::input::dotgraph::DotGraph;
 use crate::input::EdgeList;
@@ -11,7 +12,11 @@ pub type DirectedNodeLabeledCsrGraph<Node, Label> =
 pub type UndirectedNodeLabeledCsrGraph<Node, Label> =
     NodeLabeledCsrGraph<UndirectedCsrGraph<Node>, Label>;
 
-pub trait NodeLabeledGraph<Node: Idx, Label: Idx>: Graph<Node> {
+pub trait NodeLabeledGraph<Node, Label>: Graph<Node>
+where
+    Node: Idx,
+    Label: Idx,
+{
     fn label(&self, node: Node) -> Label;
 
     fn nodes_by_label(&self, label: Label) -> &[Node];
@@ -27,7 +32,9 @@ pub trait NodeLabeledGraph<Node: Idx, Label: Idx>: Graph<Node> {
 
 pub struct NodeLabeledCsrGraph<G, Label: Idx> {
     graph: G,
-    labels: Csr<Label>,
+    label_count: usize,
+    labels: Box<[Label]>,
+    label_index: Csr<Label>,
     max_degree: usize,
     max_label: Label,
     max_label_frequency: usize,
@@ -35,7 +42,7 @@ pub struct NodeLabeledCsrGraph<G, Label: Idx> {
     neighbor_label_frequencies: Option<Box<[HashMap<Label, usize>]>>,
 }
 
-impl<Node, Label, G> Graph<Node> for NodeLabeledCsrGraph<G, Label>
+impl<G, Node, Label> Graph<Node> for NodeLabeledCsrGraph<G, Label>
 where
     Node: Idx,
     Label: Idx,
@@ -52,7 +59,7 @@ where
     }
 }
 
-impl<Node, Label, G> DirectedGraph<Node> for NodeLabeledCsrGraph<G, Label>
+impl<G, Node, Label> DirectedGraph<Node> for NodeLabeledCsrGraph<G, Label>
 where
     Node: Idx,
     Label: Idx,
@@ -75,7 +82,7 @@ where
     }
 }
 
-impl<Node, Label, G> UndirectedGraph<Node> for NodeLabeledCsrGraph<G, Label>
+impl<G, Node, Label> UndirectedGraph<Node> for NodeLabeledCsrGraph<G, Label>
 where
     Node: Idx,
     Label: Idx,
@@ -90,7 +97,41 @@ where
     }
 }
 
-impl<Node, Label, G> From<(DotGraph<Node, Label>, CsrLayout)> for NodeLabeledCsrGraph<G, Label>
+impl<G, Node, Label> NodeLabeledGraph<Node, Label> for NodeLabeledCsrGraph<G, Label>
+where
+    Node: Idx + Hash,
+    Label: Idx + Hash,
+    G: Graph<Node>,
+{
+    fn label(&self, node: Node) -> Label {
+        self.labels[node.index()]
+    }
+
+    fn nodes_by_label(&self, label: Label) -> &[Node] {
+        todo!()
+    }
+
+    fn label_count(&self) -> usize {
+        self.label_count
+    }
+
+    fn max_label(&self) -> Label {
+        self.max_label
+    }
+
+    fn max_label_frequency(&self) -> usize {
+        self.max_label_frequency
+    }
+
+    fn neighbor_label_frequency(&self, node: Node) -> &HashMap<Label, usize> {
+        match &self.neighbor_label_frequencies {
+            Some(nlfs) => &nlfs[node.index()],
+            None => panic!("Neighbor label frequencies have not been loaded."),
+        }
+    }
+}
+
+impl<G, Node, Label> From<(DotGraph<Node, Label>, CsrLayout)> for NodeLabeledCsrGraph<G, Label>
 where
     Node: Idx,
     Label: Idx,
@@ -103,6 +144,8 @@ where
 
 #[cfg(test)]
 mod tests {
+    use std::path::PathBuf;
+
     use crate::{builder::GraphBuilder, input::dotgraph::DotGraphInput, Error};
 
     use super::*;
@@ -124,5 +167,24 @@ mod tests {
         }
 
         assert!(inner_test().is_err())
+    }
+
+    #[test]
+    fn from_file_test() {
+        let path = [env!("CARGO_MANIFEST_DIR"), "resources", "test.graph"]
+            .iter()
+            .collect::<PathBuf>();
+
+        let g: DirectedNodeLabeledCsrGraph<usize, usize> = GraphBuilder::new()
+            .file_format(DotGraphInput::default())
+            .path(path)
+            .build()
+            .unwrap();
+
+        assert_eq!(g.node_count(), 5);
+        assert_eq!(g.edge_count(), 6);
+        assert_eq!(g.label_count(), 5);
+        assert_eq!(g.max_label(), 2);
+        assert_eq!(g.max_degree, 3);
     }
 }
