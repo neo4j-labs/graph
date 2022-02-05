c5c68065233a34776ea81aeee7d4f1f89d2d8562
Martin Junghanns <m.junghanns@mailbox.org>
2022-02-05 23:09:12 +0100

    Add logging to server
    Co-Authored-By: Lars Hoogestraat <inbox-github@hoogi.eu>

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -2,13 +2,16 @@ mod actions;
 mod server;
 
 use arrow_flight::flight_service_server::FlightServiceServer;
+use log::info;
 use tonic::transport::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
     let addr = "[::1]:50051".parse()?;
     let service = crate::server::FlightServiceImpl::new();
     let service = FlightServiceServer::new(service);
+    info!("Starting server at {addr}");
     Server::builder().add_service(service).serve(addr).await?;
 
     Ok(())
