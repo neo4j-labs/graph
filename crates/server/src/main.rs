9153a70d8152850309848f17cca37a7b1eb1a390
Martin Junghanns <m.junghanns@mailbox.org>
2022-02-18 12:56:27 +0100

    Add README.md for the server

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,5 +1,18 @@
 #![feature(vec_into_raw_parts)]
 
+//! An [Arrow Flight](https://arrow.apache.org/docs/format/Flight.html) server
+//! implementation that allows clients to create and manage graphs in memory,
+//! run algorithms on them and stream results back to the client.
+//!
+//! Clients communicate with the server via an Arrow Flight client, such as
+//! [pyarrow](https://pypi.org/project/pyarrow/). Server commands, also called
+//! Flight actions, are encoded via JSON. Currently supported commands include
+//! creating graphs, relabeling graphs and computing algorithms, such as PageRank,
+//! Triangle Count and SSSP. Algorithm results are streamed to the client via
+//! the do_get command and nicely wrapped in Arrow record batches.
+//!
+//! Check the `examples` folder for scripts that demonstrate client-server interaction.
+
 mod actions;
 mod catalog;
 mod server;
