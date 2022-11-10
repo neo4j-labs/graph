af175e2cff80ace0a35ecc1d6b5ae46b4fdbaaa5
Martin Junghanns <m.junghanns@mailbox.org>
2022-11-10 14:30:41 +0100

    Extend page-rank.py example

--- crates/server/examples/page-rank.py
+++ crates/server/examples/page-rank.py
@@ -17,6 +17,17 @@ def flight_result(f):
     return wrapper
 
 
+def run_algorithm(client, graph_name, algo_config, property_key):
+    compute_action = {
+        "graph_name": graph_name,
+        "algorithm": algo_config,
+        "property_key": property_key,
+    }
+    return client.do_action(
+        flight.Action("compute", json.dumps(compute_action).encode("utf-8"))
+    )
+
+
 def result_to_json(result):
     return json.loads(next(result).body.to_pybytes().decode())
 
@@ -42,49 +53,111 @@ def list_graphs(client):
 
 
 @flight_result
-def run_page_rank(client, graph_name):
-    compute_action = {
-        "graph_name": graph_name,
-        "algorithm": {
+def run_page_rank(client, graph_name, property_key="page_rank"):
+    return run_algorithm(
+        client,
+        graph_name,
+        {
             "PageRank": {
                 "max_iterations": 20,
                 "tolerance": 0.0001,
                 "damping_factor": 0.85,
             }
         },
-        "property_key": "page_rank",
-    }
-    return client.do_action(
-        flight.Action("compute", json.dumps(compute_action).encode("utf-8"))
+        property_key,
     )
 
 
-def load_page_rank_result(client, ticket):
-    print_step("load_page_rank_result")
+@flight_result
+def run_wcc(client, graph_name, property_key="component"):
+    return run_algorithm(
+        client,
+        graph_name,
+        {
+            "Wcc": {
+                "chunk_size": 16384,
+                "neighbor_rounds": 2,
+                "sampling_size": 1024,
+            }
+        },
+        property_key,
+    )
+
+
+@flight_result
+def run_triangle_count(client, graph_name, property_key="triangle_count"):
+    return run_algorithm(
+        client,
+        graph_name,
+        {
+            "TriangleCount": None
+        },
+        property_key,
+    )
+
+
+def load_property(client, ticket):
+    print_step(f"load_property `{ticket}`")
     start = time.time()
     reader = client.do_get(flight.Ticket(json.dumps(ticket).encode("utf-8")))
-    scores = reader.read_all().to_pandas()
-    nbytes = scores.memory_usage().sum()
+    result = reader.read_all().to_pandas()
+    nbytes = result.memory_usage().sum()
     size_total, unit_total = convert_bytes(nbytes)
     elapsed = time.time() - start
     thrpt, unit = convert_bytes(nbytes / elapsed)
-    count = scores.count(axis=0)["page_rank"]
+    count = result.size
     print(
         f"Received {count:,} rows ({size_total} {unit_total}) in {elapsed:.2f} seconds ({thrpt} {unit}/s)"
     )
-    return scores
+    return result
 
 
 def print_page_rank_stats(scores):
     print_step("print_page_rank_stats")
     start = time.time()
-    print("size = {:,}".format(scores.size))
-    print("min = {}".format(scores.min()["page_rank"]))
-    print("max = {}".format(scores.max()["page_rank"]))
-    print("mean = {}".format(scores.mean()["page_rank"]))
-    print("median = {}".format(scores.median()["page_rank"]))
+    print(f"size = {scores.size:,}")
+    print(f"min = {scores.min()['page_rank']}")
+    print(f"max = {scores.max()['page_rank']}")
+    print(f"mean = {scores.mean()['page_rank']}")
+    print(f"median = {scores.median()['page_rank']}")
     elapsed = time.time() - start
     print(f"Computed stats locally in {elapsed:.2f} seconds")
+
+
+def print_wcc_stats(components):
+    print_step("print_wcc_stats")
+    start = time.time()
+    print(f"size = {components.size:,}")
+    unique_components = components.drop_duplicates()
+    print(f"component count = {unique_components.size:,}")
+    elapsed = time.time() - start
+    print(f"Computed stats locally in {elapsed:.2f} seconds")
+
+
+def print_tc_stats(triangle_count):
+    print_step("print_tc_stats")
+    print(f"global triangle count = {triangle_count:,}")
+
+
+@flight_result
+def to_undirected_graph(client, graph_name, csr_layout="Unsorted"):
+    to_undirected_action = {
+        "graph_name": graph_name,
+        "csr_layout": csr_layout,
+    }
+    return client.do_action(
+        flight.Action("to_undirected", json.dumps(to_undirected_action).encode("utf-8"))
+    )
+
+
+@flight_result
+def to_relabeled_graph(client, graph_name):
+    to_relabeled_action = {
+        "graph_name": graph_name,
+    }
+    return client.do_action(
+        flight.Action("to_relabeled", json.dumps(to_relabeled_action).encode("utf-8"))
+    )
 
 
 @flight_result
@@ -131,10 +204,19 @@ def main() -> int:
 
     result = run_page_rank(client, graph_name)
     ticket = result["property_id"]
-
-    scores = load_page_rank_result(client, ticket)
-
+    scores = load_property(client, ticket)
     print_page_rank_stats(scores)
+
+    result = run_wcc(client, graph_name)
+    ticket = result["property_id"]
+    components = load_property(client, ticket)
+    print_wcc_stats(components)
+
+    to_undirected_graph(client, graph_name, "Deduplicated")
+    to_relabeled_graph(client, graph_name)
+
+    result = run_triangle_count(client, graph_name)
+    print_tc_stats(result["triangle_count"])
 
     remove_graph(client, graph_name)
 
