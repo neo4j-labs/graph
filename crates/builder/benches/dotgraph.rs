83156141b72891749aa644aeb89b71d203fee8fb
Martin Junghanns <m.junghanns@mailbox.org>
2023-02-17 16:56:00 +0100

    Add benchmark for building LabelStats

--- crates/builder/benches/dotgraph.rs
+++ crates/builder/benches/dotgraph.rs
@@ -7,6 +7,28 @@ use common::*;
 use graph_builder::input::dotgraph::{LabelStats, NodeLabelIndex};
 use rand::Rng;
 
+fn label_stats(c: &mut Criterion) {
+    let mut group = c.benchmark_group("label_stats");
+
+    group.sampling_mode(SamplingMode::Flat);
+
+    group.bench_function(SMALL.name, |b| bench_label_stats(b, SMALL));
+    group.bench_function(MEDIUM.name, |b| bench_label_stats(b, MEDIUM));
+    group.bench_function(LARGE.name, |b| bench_label_stats(b, LARGE));
+
+    group.finish();
+}
+
+fn bench_label_stats(b: &mut criterion::Bencher, Input { node_count, .. }: Input) {
+    let labels = common::gen::node_values(node_count, |_node, rng| rng.gen_range(0..42));
+    let graph: UndirectedCsrGraph<usize, usize> = GraphBuilder::new()
+        .edges([(0, node_count - 1)])
+        .node_values(labels.clone())
+        .build();
+
+    b.iter(|| black_box(LabelStats::from_graph(&graph)))
+}
+
 fn node_label_index(c: &mut Criterion) {
     let mut group = c.benchmark_group("node_label_index");
     group.sampling_mode(SamplingMode::Flat);
