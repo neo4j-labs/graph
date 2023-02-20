6c2e87f5af2bf913defd590c4746cfa824b3aafe
Martin Junghanns <m.junghanns@mailbox.org>
2023-02-20 17:53:14 +0100

    Add util for downloading and decompressing graph500 graphs
    Co-Authored-By: Paul Horn <dev@knutwalker.engineer>

--- crates/bench-util/src/lib.rs
+++ crates/bench-util/src/lib.rs
@@ -1,8 +1,65 @@
 #![allow(dead_code)]
+use std::{
+    ffi::OsStr,
+    path::{Path, PathBuf},
+};
+
+use directories::ProjectDirs;
+use futures::{StreamExt, TryStreamExt};
 
 use graph_builder::prelude::Idx;
 use rand::prelude::*;
 
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+const GRAPHALYTICS: &str = "https://pub-383410a98aef4cb686f0c7601eddd25f.r2.dev/graphalytics/";
+
+/// Downloads a Graph 500 edge list from
+/// [LDBC](https://ldbcouncil.org/benchmarks/graphalytics/)
+/// and decompresses it into the application cache
+/// directory.
+///
+/// The file is only downloaded if it's not already present.
+///
+/// Available scale factors are 22..=30
+pub fn create_graph_500(scale: usize) -> Result<PathBuf> {
+    let proj_dirs = ProjectDirs::from("", "", "graph").unwrap();
+
+    let cache_dir = proj_dirs.cache_dir();
+    let download_dir = cache_dir
+        .join("datasets")
+        .join(format!("graph-500-{scale}"));
+    if !download_dir.exists() {
+        std::fs::create_dir_all(&download_dir)?;
+        let from = format!("{GRAPHALYTICS}graph500-{scale}.tar.zst");
+
+        let rt = tokio::runtime::Runtime::new()?;
+        rt.block_on(async { download_and_decompress(&from, &download_dir).await })?;
+    }
+
+    Ok(download_dir.join(format!("graph500-{}.e", scale)))
+}
+
+async fn download_and_decompress<P: AsRef<Path>>(url: &str, download: P) -> Result<()> {
+    let response = reqwest::get(url).await?.bytes_stream();
+    let response = response.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e));
+    let bufread = tokio_util::io::StreamReader::new(response);
+    let bufread = async_compression::tokio::bufread::ZstdDecoder::new(bufread);
+    let bufread = tokio::io::BufReader::new(bufread);
+    let mut tar_archive = tokio_tar::Archive::new(bufread);
+    let mut entries = tar_archive.entries()?;
+
+    while let Some(file) = entries.next().await {
+        let mut f = file?;
+        let path = f.path()?;
+
+        if path.extension() == Some(OsStr::new("e")) {
+            f.unpack_in(&download).await?;
+        }
+    }
+
+    Ok(())
+}
 
 #[derive(Clone, Copy)]
 pub struct Input {
