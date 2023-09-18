d85b789c836336088e33c18f9c1dca69dad614f8
Martin Junghanns <m.junghanns@mailbox.org>
2023-09-18 12:01:17 +0200

    Add AdjacencyList target methods

--- crates/builder/src/graph/adj_list.rs
+++ crates/builder/src/graph/adj_list.rs
@@ -35,6 +35,33 @@ impl<NI: Idx, EV> AdjacencyList<NI, EV> {
     }
 }
 
+impl<NI: Idx, EV> AdjacencyList<NI, EV> {
+    #[inline]
+    pub(crate) fn targets_with_values(&self, node: NI) -> &[Target<NI, EV>] {
+        self.edges[node.index()].as_slice()
+    }
+}
+
+impl<NI: Idx> AdjacencyList<NI, ()> {
+    #[inline]
+    pub(crate) fn targets(&self, node: NI) -> &[NI] {
+        assert_eq!(
+            std::mem::size_of::<Target<NI, ()>>(),
+            std::mem::size_of::<NI>()
+        );
+        assert_eq!(
+            std::mem::align_of::<Target<NI, ()>>(),
+            std::mem::align_of::<NI>()
+        );
+
+        let targets = self.edges[node.index()].as_slice();
+
+        // SAFETY: The types Target<T, ()> and T are verified to have the same
+        //         size and alignment.
+        unsafe { std::slice::from_raw_parts(targets.as_ptr() as *const NI, targets.len()) }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -47,7 +74,7 @@ mod test {
     }
 
     #[test]
-    fn simple_graph() {
+    fn degree() {
         let list = AdjacencyList::<u32, u32>::new(vec![
             /* node 0 */ vec![Target::new(1, 42)],
             /* node 1 */ vec![Target::new(0, 1337)],
@@ -56,5 +83,27 @@ mod test {
         assert_eq!(list.edge_count(), 2);
         assert_eq!(list.degree(0), 1);
         assert_eq!(list.degree(1), 1);
+    }
+
+    #[test]
+    fn targets_with_values() {
+        let list = AdjacencyList::<u32, u32>::new(vec![
+            /* node 0 */ vec![Target::new(1, 42)],
+            /* node 1 */ vec![Target::new(0, 1337)],
+        ]);
+
+        assert_eq!(list.targets_with_values(0), &[Target::new(1, 42)]);
+        assert_eq!(list.targets_with_values(1), &[Target::new(0, 1337)]);
+    }
+
+    #[test]
+    fn targets() {
+        let list = AdjacencyList::<u32, ()>::new(vec![
+            /* node 0 */ vec![Target::new(1, ())],
+            /* node 1 */ vec![Target::new(0, ())],
+        ]);
+
+        assert_eq!(list.targets(0), &[1]);
+        assert_eq!(list.targets(1), &[0]);
     }
 }
